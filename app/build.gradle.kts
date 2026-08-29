5c462deaf5757c70f34ef6d9c4141a3522b7ec7a
AdministratorProgrammer <aleksandrsupp0rt@gmail.com>
2026-08-29 14:15:56 +0400

    add proguard rules

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,6 +20,11 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
