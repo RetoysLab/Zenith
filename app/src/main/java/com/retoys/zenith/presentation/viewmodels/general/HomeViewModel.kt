291057226ba08d4a45ebb71e7e09c2ebeffe8475
AdministratorProgrammer <aleksandrsupp0rt@gmail.com>
2026-08-29 15:00:13 +0400

    clear HomeViewModel (refactoring)

---
+++ app/src/main/java/com/retoys/zenith/presentation/viewmodels/general/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.retoys.zenith.presentation.viewmodels.general
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor() : ViewModel() {
+
+    init {
+        //
+    }
+}
